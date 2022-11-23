516b24686dd89c56395ee078f6701d0c4fd6b1ec
Majewska <marta.majewska@yougov.com>
2022-11-23 17:12:34 +0100

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'svelte-web-component',
+      fileName: 'svelte-web-component',
+    },
+  },
+  plugins: [svelte()]
+})
