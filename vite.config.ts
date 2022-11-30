6b68eb924ad5e7d3aeefc0fa0e3adb0e8f9f7002
Majewska <marta.majewska@yougov.com>
2022-11-30 14:58:11 +0100

    Add autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { resolve } from 'path'
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [autoprefixer]
+    }
+  },
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
@@ -12,5 +18,5 @@ export default defineConfig({
       fileName: 'svelte-web-component',
     },
   },
-  plugins: [svelte(), cssInjectedByJsPlugin(),]
+  plugins: [svelte(), cssInjectedByJsPlugin()]
 })
