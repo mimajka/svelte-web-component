10787ef3c9418987aa30141546e3cb036efac356
Majewska <marta.majewska@yougov.com>
2022-11-30 11:16:19 +0100

    Add sass and inject styles to js file

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -5,6 +5,35 @@
   }
 </script>
 
+<style lang="scss">
+  button {
+    border-radius: 8px;
+    border: 1px solid transparent;
+    padding: 0.6em 1.2em;
+    font-size: 1em;
+    font-weight: 500;
+    font-family: inherit;
+    background-color: #1a1a1a;
+    cursor: pointer;
+    transition: border-color 0.25s;
+
+    &:hover {
+      border-color: #646cff;
+    }
+
+    &:focus, &:focus-visible {
+      border-color: #646cff;
+      outline: 4px auto -webkit-focus-ring-color;
+    }
+  }
+
+  @media (prefers-color-scheme: light) {
+    button {
+      background-color: #f9f9f9;
+    }
+  }
+</style>
+
 <button on:click={increment}>
   count is {count}
 </button>
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { resolve } from 'path'
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/main.ts'),
       name: 'SvelteWebComponent',
       fileName: 'svelte-web-component',
     },
   },
-  plugins: [svelte()]
+  plugins: [svelte(), cssInjectedByJsPlugin(),]
 })
